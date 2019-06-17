8ee60a95a84e3998c2e3a59b390cc07eaef6657c
Unknown <l365692902@gmail.com>
2019-06-17 22:56:36 +0800

    屏蔽百度搜索热点

---
+++ 屏蔽百度搜索热点.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         屏蔽百度搜索热点
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  百度作为业界毒瘤，其搜索热点大多是低俗内容。该脚本会屏蔽这些内容，彰显格调，品味。
+// @author       c4r
+// @match        *://www.baidu.com/s?*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    GM_addStyle(`
+        #content_right {
+            display: none;
+        }
+    `)
+    'use strict';
+    (function(open) {
+        XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {
+            this.addEventListener("readystatechange", function() {
+                //console.warn('UI', this.readyState)
+                if(this.readyState == 1){
+                    document.getElementById('content_right').style.display = 'none'
+                }
+            }, false);
+            open.call(this, method, url, async, user, pass);
+        };
+    })(XMLHttpRequest.prototype.open);
+})();
